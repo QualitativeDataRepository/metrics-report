d00c0af7eb63ce7c7e00fb22aed901a1e5465397
mccallc <26819445+mccallc@users.noreply.github.com>
2022-03-08 10:18:08 -0500

    add helper function

---
+++ helper.R
@@ -0,0 +1,39 @@
+library(stringr)
+
+checkif_member <- function(login) {
+  institutions <- structure(list(name = c("American", "Brown", "Case Western", 
+                                          "Columbia", "Cornell", "Duke", "Fordham", "George Mason", "George Washington", 
+                                          "Macalester", "Marine Corps", "3ie", "New York", "Palliative Care Research Cooperative", 
+                                          "Princeton", "Syracuse", "California, Berkeley", "California, Merced", 
+                                          "Cincinnati", "Iowa", "Maryland, Baltimore", "Michigan, Ann Arbor", 
+                                          "New Mexico", "North Carolina, Chapel Hill", "Texas, Austin", 
+                                          "Villanova", "Virginia Tech", "West Virginia"), 
+                                 domain = c("american.edu", 
+                                            "brown.edu", "case.edu", "columbia.edu", "cornell.edu", "duke.edu", 
+                                            "fordham.edu", "gmu.edu", "gwu.edu", "macalester.edu", "usmcu.edu", 
+                                            "3ieimpact.org", "nyu.edu", "palliativecareresearch.org", "princeton.edu", 
+                                            "syr.edu", "berkeley.edu", "ucmerced.edu", "uc.edu", "uiowa.edu", 
+                                            "umaryland.edu", "umich.edu", "unm.edu", "unc.edu", "utexas.edu", 
+                                            "villanova.edu", "vt.edu", "wvu.edu")), 
+                            class = "data.frame", row.names = c(NA, -28L))
+  
+  login$Institution <- str_remove(login$Institution, " UNIVERSITY$")
+  login$Institution <- str_remove(login$Institution, "^UNIVERSITY OF ")
+  login$`Name/Email` <- str_extract(login$`Name/Email`, "[a-zA-Z0-9]+\\.edu$")
+  
+  institution_match <- unlist(lapply(login$Institution, function(x, name) {
+    match <- str_which(name, paste0("^", x, "$"))
+    if (length(match)==0) return(NA)
+    return(match)
+  }, str_to_upper(institutions$name))) # Need str_to_upper because we uppercase in the Rmd
+  
+  email_match <- unlist(lapply(login$`Name/Email`, function(x, domain) {
+    match <- str_which(domain, paste0(x, "$"))
+    if (length(match)==0) return(NA) 
+    return(match[1])
+  }, str_to_lower(institutions$domain)))
+  
+  matches <- dplyr::coalesce(institution_match, email_match)
+  matches <- factor(matches, levels = 1:nrow(institutions), labels = institutions$name)
+  return(matches)
+}
